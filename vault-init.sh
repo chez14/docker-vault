ba299fb1f7345d0acc0babbc836509718a487a6d
Chris "chez14" Qiang <chris@christianto.net>
2022-03-27 23:32:22 +0700

    Vault init to use other folder for better clarity

--- vault-init.sh
+++ vault-init.sh
@@ -3,17 +3,17 @@
 set -ex
 
 unseal () {
-vault operator unseal $(grep 'Key 1:' /vault/file/keys | awk '{print $NF}')
-vault operator unseal $(grep 'Key 2:' /vault/file/keys | awk '{print $NF}')
-vault operator unseal $(grep 'Key 3:' /vault/file/keys | awk '{print $NF}')
+vault operator unseal $(grep 'Key 1:' /vault/_key-token/keys | awk '{print $NF}')
+vault operator unseal $(grep 'Key 2:' /vault/_key-token/keys | awk '{print $NF}')
+vault operator unseal $(grep 'Key 3:' /vault/_key-token/keys | awk '{print $NF}')
 }
 
 init () {
-vault operator init > /vault/file/keys
+vault operator init > /vault/_key-token/keys
 }
 
 log_in () {
-   export ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/file/keys | awk '{print $NF}')
+   export ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/_key-token/keys | awk '{print $NF}')
    vault login $ROOT_TOKEN
 }
 
@@ -21,7 +21,7 @@ create_token () {
    vault token create -id $MY_VAULT_TOKEN
 }
 
-if [ -s /vault/file/keys ]; then
+if [ -s /vault/_key-token/keys ]; then
    unseal
 else
    init
